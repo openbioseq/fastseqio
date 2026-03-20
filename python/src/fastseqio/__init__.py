79230798f5b4aab1d422ba4c7a8f9a70f2ee240d
dwpeng <1732889554@qq.com>
2026-03-20 14:27:45 +0800

    add a wrapper function `open` for seqioFile init.

--- python/src/fastseqio/__init__.py
+++ python/src/fastseqio/__init__.py
@@ -1,3 +1,12 @@
+from typing import Literal
+
 from .fastseqio import seqioFile, Record
 
-__all__ = ["seqioFile", "Record"]
+__all__ = ["seqioFile", "Record", "open"]
+
+
+def open(
+    path: str, mode: Literal["w", "r"] = "r", compressed: bool = False
+) -> seqioFile:
+    return seqioFile(path, mode, compressed)
+
